5244c7b3cefb9f5fc64c05fd112397f19f4695b2
Carsten Duch <carsten@hashicorp.com>
2021-10-27 17:42:52 +0200

    added port forwarding help

--- 02_port_forward.sh
+++ 02_port_forward.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-kubectl --namespace consul port-forward service/consul-ui 8080:80 --address 0.0.0.0
-
-kubectl --namespace consul port-forward consul-server-0 8500:8500
+echo "please open two separate CLIs and use the following commands for port forwarding in each of the CLIs:"
+echo "kubectl --namespace consul port-forward service/consul-ui 8080:80 --address 0.0.0.0"
+echo "kubectl --namespace consul port-forward consul-server-0 8500:8500"
