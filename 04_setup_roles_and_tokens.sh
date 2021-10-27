48b52e269517561a3fca600f3ca0d62cee129a45
Carsten Duch <carsten@hashicorp.com>
2021-10-27 17:57:24 +0200

    added token explanation

--- 04_setup_roles_and_tokens.sh
+++ 04_setup_roles_and_tokens.sh
@@ -6,6 +6,18 @@ consul acl role create -name "team1" -description "team1 role" -policy-name "tea
 consul acl role create -name "team2" -description "team2 role" -policy-name "team2"
 consul acl role create -name "team3" -description "team3 role" -policy-name "team3"
 
+echo "=========================================================================="
+echo "Create the Tokens for the different teams:"
+echo "=========================================================================="
 consul acl token create -description "Team1 Admin" -role-name team1
+echo "=========================================================================="
 consul acl token create -description "Team2 Admin" -role-name team2
+echo "=========================================================================="
 consul acl token create -description "Team3 Admin" -role-name team3
+echo "=========================================================================="
+
+echo "Make sure to write down the above secretIDs for each Team Admin to test the access via UI!"
+echo "http://localhost:8080/ui/"
+echo ""
+echo "You can also use the bootstrap token for 'superadmin' access."
+echo "As a reminder, your bootstrap token is: $CONSUL_HTTP_TOKEN"
